67804d3a000e3627507acbdfe573c1d36f3a0156
Frank <a20213732@pucp.edu.pe>
2026-04-13 22:24:04 -0500

    WIP

---
+++ algoritmoGenetico/genetico/src/main/java/com/tasf_b2b/planificador/dominio/GeneticoApplication.java
@@ -0,0 +1,13 @@
+package com.tasf_b2b.planificador.dominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneticoApplication.class, args);
+	}
+
+}
